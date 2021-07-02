56176bad05b63ba03b6ccf99932cf908f82b76be
leozcarvalho <64977020+leozcarvalho@users.noreply.github.com>
2021-07-02 18:12:54 -0300

    Add files via upload

---
+++ client/src/components/Books.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-10">
+        <h1>Livros</h1>
+        <hr><br><br>
+        <button type="button" class="btn btn-success btn-sm">Adicionar Livro</button>
+        <br><br>
+        <table class="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">Título</th>
+              <th scope="col">Autor</th>
+              <th scope="col">Lido?</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(book, index) in books" :key="index">
+              <td>{{ book.title }}</td>
+              <td>{{ book.author }}</td>
+              <td>
+                <span v-if="book.read">Sim</span>
+                <span v-else>Não</span>
+              </td>
+              <td>
+                <div class="btn-group" role="group">
+                  <button type="button" class="btn btn-warning btn-sm">Atualizar</button>
+                  <button type="button" class="btn btn-danger btn-sm">Excluir</button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      books: [],
+    };
+  },
+  methods: {
+    getBooks() {
+      const path = 'http://localhost:5000/books';
+      axios.get(path)
+        .then((res) => {
+          this.books = res.data.books;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+    },
+  },
+  created() {
+    this.getBooks();
+  },
+};
+</script>
